09e19b65003b82d2dffac7c56c7513e2622103fd
jackie.scl <jackie.scl@webi.com.cn>
2019-03-24 16:00:34 +0800

    交换机类型direct 消费者监听变更，新增消费者消息确认

--- src/main/java/config/RabbitConfig.java
+++ src/main/java/config/RabbitConfig.java
@@ -8,6 +8,8 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,38 +21,48 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 @EnableRabbit
 public class RabbitConfig {
-	 	@Bean
-	    public ConnectionFactory connectionFactory() {
-	 		CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-			connectionFactory.setHost("localhost");
-	 		connectionFactory.setUsername("jackie");
-	 		connectionFactory.setPassword("jackie");
-	 		connectionFactory.setPort(5672);
-			/** 如果要进行消息回调，则这里必须要设置为true */
-			connectionFactory.setPublisherConfirms(true);
-	 		return connectionFactory;
-	    }
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setUsername("jackie");
+        connectionFactory.setPassword("jackie");
+        connectionFactory.setPort(5672);
+        /** 如果要进行消息回调，则这里必须要设置为true */
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
 
-	    @Bean
-	    public AmqpAdmin amqpAdmin() {
-	        return new RabbitAdmin(connectionFactory());
-	    }
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
 
-	    @Bean
-		/** 因为要设置回调类，所以应是prototype类型，如果是singleton类型，则回调类为最后一次设置 */
-		@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-	    public RabbitTemplate rabbitTemplate() {
-	        return new RabbitTemplate(connectionFactory());
-	    }
 
-	    //配置消费者监听的容器
-	    @Bean
-	    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
-	        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-			factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-	        factory.setConnectionFactory(connectionFactory());
-	        factory.setConcurrentConsumers(3);
-	        factory.setMaxConcurrentConsumers(10);
-	        return factory;
-	    }
+    @Bean
+    /** 因为要设置回调类，所以应是prototype类型，如果是singleton类型，则回调类为最后一次设置 */
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    //配置消费者监听的容器
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
 }
--- src/main/java/rabbitMQ/listener/ProducerConsumerListenerTwo.java
+++ src/main/java/rabbitMQ/listener/ProducerConsumerListenerTwo.java
@@ -1,15 +1,17 @@
 package rabbitMQ.listener;
 
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import po.Mail;
 @Component
 public class ProducerConsumerListenerTwo {
 	
 	@RabbitListener(queues = "myQueue")
-	public void displayMail(Mail mail) throws Exception {
-		System.out.println("队列监听器2号收到消息"+mail.toString());
+	public void displayMail(Channel channel, Message message) throws Exception {
+		System.out.println("队列监听器2号收到消息 start:\n" + new String(message.getBody(), "UTF-8"));
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+		System.out.println("队列监听器2号收到消息 end");
 	}
 }
